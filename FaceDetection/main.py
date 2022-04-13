c9faadd4085b8777c34ee494c24ae46b2703dca4
Dylan Marczak <starmarczak@gmail.com>
2022-04-12 22:49:15 -0500

    added facedetection

---
+++ FaceDetection/main.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+print("hello")
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
